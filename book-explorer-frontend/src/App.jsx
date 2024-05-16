9e47685937d3c4471510305257637f2e405163c5
Oscar Kim <oscarkim32@gmail.com>
2024-05-15 23:29:09 -0700

    completed the main logic for the pagination bar

--- book-explorer-frontend/src/App.jsx
+++ book-explorer-frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import BooksContainer from "./components/BooksContainer";
+import PaginationBar from "./components/PaginationBar";
 function App() {
   return (
     <div>
       <SearchBar></SearchBar>
       <BooksContainer></BooksContainer>
+      <PaginationBar></PaginationBar>
     </div>
   );
 }
